371af7ed8ced5d36f66b4250de28f8f6aafc9210
Itziar Aldabe Arregi <ialdabe@gmail.com>
2013-05-09 11:19:16 +0200

    install.sh => control modifications

--- install.sh
+++ install.sh
@@ -1,21 +1,34 @@
 #!/bin/tcsh
 
+set SPOTLIGHTDIR = "dbpedia-spotlight";
+
+cd .. 
+
+if (-d $SPOTLIGHTDIR)
+    echo "dbpedia spotlight already exists skipping the step of downloading it";
+else
+    echo "downloading dbepdia spotlight";
+    git clone https://github.com/dbpedia-spotlight/dbpedia-spotlight.git
+
+cd $SPOTLIGHTDIR
+
+if (-e pom.xml.orig)
+    echo "the original pom.xml files have been already replaced"
+else
+    mv pom.xml pom.xml.orig
+    mv core/pom.xml core/pom.xml.orig
+    cd ../IXA-EHU-DBpedia-spotlight
+    cp pom.xml ../dbpedia-spotlight/.
+    cp core/pom.xml ../dbpedia-spotlight/core/.
+    cp conf/server_en.properties ../dbpedia-spotlight/conf/.
+    cp conf/server_es.properties ../dbpedia-spotlight/conf/.
+
 cd ..
-git clone https://github.com/dbpedia-spotlight/dbpedia-spotlight.git
-
-cd dbpedia-spotlight
-mv pom.xml pom.xml.orig
-mv core/pom.xml core/pom.xml.orig
-
-cd ../IXA-EHU-DBpedia-spotlight
-cp pom.xml ../dbpedia-spotlight/.
-cp core/pom.xml ../dbpedia-spotlight/core/.
-cp conf/server_en.properties ../dbpedia-spotlight/conf/.
-cp conf/server_es.properties ../dbpedia-spotlight/conf/.
-
-cd ../dbpedia-spotlight
+cd $SPOTLIGHTDIR
+echo "installing the modified dbpedia spolight"
 mvn clean install
 
+echo "creating the jar with dependencies"
 cd dist
 mvn clean package
 
